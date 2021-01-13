9385abfe4020561d1a4b9b8e750a6edcd7502c7c
ChinChuanKuo <chinchuankuo@jerryshyu1.flytech.com.tw>
2021-01-13 11:40:04 +0800

    module better

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -42,7 +42,11 @@ namespace duretoryApi.Controllers
                 {
                     await Request.Form.Files[0].CopyToAsync(fileStream);
                     //string src = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1" && mainRows.Rows[0]["flShowed"].ToString().TrimEnd() == "0" ? $"{database.connectionString("sysFiles")}{mainRows.Rows[0]["original"].ToString().TrimEnd()}({mainRows.Rows[0]["encryption"].ToString().TrimEnd()}){mainRows.Rows[0]["extension"].ToString().TrimEnd()}" : $"{filePath}{original}({encryption}){extension}";
-                    return Json(new sSiteModels() { images = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1", videos = mainRows.Rows[0]["flVideos"].ToString().TrimEnd() == "1", audios = mainRows.Rows[0]["flAudios"].ToString().TrimEnd() == "1", files = new byte[fileStream.Length], status = "istrue" });
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        fileStream.CopyTo(memoryStream);
+                        return Json(new sSiteModels() { images = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1", videos = mainRows.Rows[0]["flVideos"].ToString().TrimEnd() == "1", audios = mainRows.Rows[0]["flAudios"].ToString().TrimEnd() == "1", files = memoryStream.ToArray(), status = "istrue" });
+                    }
                 }
             }
             return Json(new sSiteModels() { status = "nodata" });
