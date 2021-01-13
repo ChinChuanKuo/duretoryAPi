bed49f4deb34582d6f7d87853b2b0488e11fe9b9
ChinChuanKuo <chinchuankuo@jerryshyu1.flytech.com.tw>
2021-01-13 12:47:57 +0800

    module better

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Collections.Generic;
 using System.IO;
+using System;
 
 namespace duretoryApi.Controllers
 {
@@ -41,8 +42,10 @@ namespace duretoryApi.Controllers
                 using (var fileStream = new FileStream($"{filePath}{original}({encryption}){extension}", FileMode.Create))
                 {
                     await Request.Form.Files[0].CopyToAsync(fileStream);
-                    string src = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1" && mainRows.Rows[0]["flShowed"].ToString().TrimEnd() == "0" ? $"{database.connectionString("sysFiles")}{mainRows.Rows[0]["original"].ToString().TrimEnd()}({mainRows.Rows[0]["encryption"].ToString().TrimEnd()}){mainRows.Rows[0]["extension"].ToString().TrimEnd()}" : $"{database.connectionString("sysHttps")}5E28F1D7-D153-430F-814D-82D6AD7C4E93/{original}({encryption}){extension}";
-                    return Json(new sSiteModels() { images = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1", videos = mainRows.Rows[0]["flVideos"].ToString().TrimEnd() == "1", audios = mainRows.Rows[0]["flAudios"].ToString().TrimEnd() == "1", files = System.IO.File.ReadAllBytes(src), status = "istrue" });
+                    //string src = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1" && mainRows.Rows[0]["flShowed"].ToString().TrimEnd() == "0" ? $"{database.connectionString("sysFiles")}{mainRows.Rows[0]["original"].ToString().TrimEnd()}({mainRows.Rows[0]["encryption"].ToString().TrimEnd()}){mainRows.Rows[0]["extension"].ToString().TrimEnd()}" : $"{filePath}{original}({encryption}){extension}";
+                    System.IO.BinaryReader binaryReader = new BinaryReader(fileStream);
+                    long byteLength = new System.IO.FileInfo(Request.Form.Files[0].FileName).Length;
+                    return Json(new sSiteModels() { images = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1", videos = mainRows.Rows[0]["flVideos"].ToString().TrimEnd() == "1", audios = mainRows.Rows[0]["flAudios"].ToString().TrimEnd() == "1", files = binaryReader.ReadBytes((Int32)byteLength), status = "istrue" });
                 }
             }
             return Json(new sSiteModels() { status = "nodata" });
