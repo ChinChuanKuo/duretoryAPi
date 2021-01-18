c13fa6483d4ce4f4a728d8c0c894390206554c36
ChinChuanKuo <chinchuankuo@nickmao2.flytech.com.tw>
2021-01-18 12:20:55 +0800

    module better

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -41,7 +41,7 @@ namespace duretoryApi.Controllers
                 using (var fileStream = new FileStream($"{filePath}{original}({encryption}){extension}", FileMode.Create))
                 {
                     await Request.Form.Files[0].CopyToAsync(fileStream);
-                    string src = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1" && mainRows.Rows[0]["flShowed"].ToString().TrimEnd() == "0" ? $"{database.connectionString("sysFiles")}{mainRows.Rows[0]["original"].ToString().TrimEnd()}({mainRows.Rows[0]["encryption"].ToString().TrimEnd()}){mainRows.Rows[0]["extension"].ToString().TrimEnd()}" : $"{filePath}{original}({encryption}){extension}";
+                    string src = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1" && mainRows.Rows[0]["flShowed"].ToString().TrimEnd() == "0" ? $"{database.connectionString("sysFiles")}{mainRows.Rows[0]["original"].ToString().TrimEnd()}({mainRows.Rows[0]["encryption"].ToString().TrimEnd()}){mainRows.Rows[0]["extension"].ToString().TrimEnd()}" : $"{database.connectionString("fileFolder")}5E28F1D7-D153-430F-814D-82D6AD7C4E93\\{original}({encryption}){extension}";
                     return Json(new sSiteModels() { images = mainRows.Rows[0]["flImages"].ToString().TrimEnd() == "1", videos = mainRows.Rows[0]["flVideos"].ToString().TrimEnd() == "1", audios = mainRows.Rows[0]["flAudios"].ToString().TrimEnd() == "1", files = System.IO.File.ReadAllBytes(src), status = "istrue" });
                 }
             }
